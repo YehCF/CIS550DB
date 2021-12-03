dced75cf25f9ae8d1c54aa73d8518566209c5e6b
paynesa <paynesa@sas.upenn.edu>
2021-12-03 17:39:56 -0500

    stacked bar chart added

--- client/src/pages/VotingPage.js
+++ client/src/pages/VotingPage.js
@@ -9,39 +9,49 @@ import {
     CardTitle,
     Container,
 } from "shards-react";
+import { DualAxes, Column } from "@ant-design/charts";
 import { Row, Col, DatePicker, Space, Divider, Table, Slider} from "antd";
 import moment from "moment";
 import ReactLoading from "react-loading";
-import { DualAxes } from "@ant-design/charts";
 import USAMap from "react-usa-map";
 import MenuBar from "../components/MenuBar";
 import {
-    getCaseAndStock,
-    getAllStocks,
-    getStocks,
-    getAllIndustries,
-    getAllStates,
-    getStateIndustry,
-    getPartyCounts,
     getPopulousVotes
 } from "../fetcher";
 
 import * as d3 from "d3";
 
-const dateFormat = "YYYY-MM-DD";
-const { RangePicker } = DatePicker;
-const default_period = ["1976", "2020"];
+/**The Bar Chart we'll use to Look at Populations*/
+const DemoColumn = (res) => {
+    //sort the data alphabetically so the x axis always stays in the same order
+    const data = res.data.sort((a, b) => a.party_detailed.localeCompare(b.party_detailed));
+    const config = {
+        data,
+        //grouped data by the count type (least vs. most populous states)
+        isGroup: true,
+        xField: "party_detailed",
+        yField: "count",
+        seriesField: "count_type",
+        // label is to show the y value in each bar
+        label: {
+            position: "top",
+            style: {
+                fill: "black",
+                opacity: 0.6,
+            },
+        },
+        //autorotate x-axis labels so that we don't end up cutting them off
+        xAxis: {
+            label: {
+                autoHide: false,
+                autoRotate: true,
+            },
+        },
+    };
+    return <Column {...config} />;
+};
 
-const nIndustryColors = 11;
-const colorScaler = d3
-    .scaleLinear()
-    .range(["#e8e8e8", "#5ac8c8"])
-    .domain([0, nIndustryColors - 1]);
-let industryColorArray = [];
-for (let i = 0; i < nIndustryColors; i++) {
-    industryColorArray[i] = colorScaler(i);
-}
-console.log(industryColorArray);
+
 
 const marks = {
     1976: '1976',
@@ -81,6 +91,7 @@ class VotingPage extends React.Component {
             maxyear: 2020,
             limitPopulous: 5,
             resultsPopulous: [],
+            resultsPopGraph: [],
             selectedState: "",
             tableLoading: false,
         }
@@ -94,10 +105,10 @@ class VotingPage extends React.Component {
             this.setState( {
                 minyear : event[0],
                 maxyear: event[1]
+            }, ()=>{
+                this.updatePopulousResults();
             });
-            getPopulousVotes(event[0], event[1], this.state.limitPopulous).then((res)=>{
-                this.setState({resultsPopulous: res.results});
-            })
+
         }
     }
 
@@ -107,15 +118,19 @@ class VotingPage extends React.Component {
 
     updatePopulousResults(event){
         getPopulousVotes(this.state.minyear, this.state.maxyear, this.state.limitPopulous).then((res)=>{
-            console.log(res.results);
-            this.setState({resultsPopulous: res.results});
+            var newGraphRes = []
+            var i = 0;
+            for (const obj of res.results){
+                newGraphRes[i] = {party_detailed: obj.party_detailed, count_type: `${this.state.limitPopulous} Least Populous States`, count: obj.least_populous_count}
+                newGraphRes[i + 1] = {party_detailed: obj.party_detailed, count_type: `${this.state.limitPopulous} Most Populous States`, count: obj.most_populous_count}
+                i += 2;
+            }
+            this.setState({resultsPopulous: res.results, resultsPopGraph: newGraphRes});
         })
     }
 
     componentDidMount() {
-        getPopulousVotes(this.state.minyear, this.state.maxyear, this.state.limitPopulous).then((res) =>{
-            this.setState({resultsPopulous: res.results});
-        });
+        this.updatePopulousResults();
     }
 
     render() {
@@ -137,7 +152,7 @@ class VotingPage extends React.Component {
                     <CardBody>
                         <CardTitle>
                             <div style={{ width: "80vw", margin: "auto auto", marginTop: "3vh"}}>
-                                <h3>Populous vs. Least Populous States</h3>
+                                <h3>Most Populous vs. Least Populous States</h3>
                             </div>
                         </CardTitle>
                         <div style={{ width: "80vw", margin: "auto auto", marginTop: "3vh"}}>
@@ -168,6 +183,9 @@ class VotingPage extends React.Component {
                                 </Col>
                             </Row>
                         </Form>
+                        <div style={{ width: "80vw", margin: "0 auto", marginTop: "5vh" }}>
+                            <DemoColumn data={this.state.resultsPopGraph} />
+                        </div>
                         <Table
                             columns={populousColumns}
                             dataSource={this.state.resultsPopulous}
@@ -179,6 +197,7 @@ class VotingPage extends React.Component {
                             }}
                             style={{ width: "80vw", margin: "0 auto", marginTop: "2vh" }}
                         />
+
                     </CardBody>
                 </Card>
 
