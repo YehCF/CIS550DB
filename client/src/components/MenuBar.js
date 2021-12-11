b678d9359434305744c06ec107ff5905a6812824
victorHLin <lin1129@seas.upenn.edu>
2021-12-11 08:35:47 +0800

    New Menu Bar

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,39 +1,72 @@
 import React from "react";
-import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "shards-react";
+
+import {
+    Navbar,
+    NavItem,
+    NavbarToggler,
+    Collapse,
+    NavLink,
+    Nav,
+    NavbarBrand
+} from 'reactstrap';
 
 class MenuBar extends React.Component {
+	constructor(props) {
+        super(props)
+        this.state = {
+            isOpen: false
+        }
+		this.handleOpen = this.handleOpen.bind(this)
+	}
+	
+	handleOpen(variable) {
+        this.setState({
+            isOpen : !variable
+          });
+    }
+	
   render() {
     return (
-      <Navbar type="dark" theme="primary" expand="md">
-        <NavbarBrand href="/">Polls, Pandemics and Possibly More!</NavbarBrand>
-        <Nav navbar>
-          <NavItem>
-            <NavLink active href="/">
-              Home
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink active href="/covid">
-              COVID19
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink active href="/vote">
-              Vote
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink active href="/stock">
-              Stock
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink active href="/yelp">
-              Yelp
-            </NavLink>
-          </NavItem>
-        </Nav>
-      </Navbar>
+	
+		<Navbar color="dark" dark >
+		
+		<div style={{
+            display: 'block', width: 550
+        }}>
+			<NavbarBrand href="/">Polls, Pandemics and Possibly More!</NavbarBrand>
+			<NavbarToggler onClick={() => this.handleOpen(this.state.isOpen)} />
+			<Collapse isOpen={this.state.isOpen} navbar>
+				<Nav className="mr-auto" navbar>
+					<NavItem>
+						<NavLink active href="/">
+						  Home
+						</NavLink>
+					  </NavItem>
+					  <NavItem>
+						<NavLink active href="/covid">
+						  COVID19
+						</NavLink>
+					  </NavItem>
+					  <NavItem>
+						<NavLink active href="/vote">
+						  Vote
+						</NavLink>
+					  </NavItem>
+					  <NavItem>
+						<NavLink active href="/stock">
+						  Stock
+						</NavLink>
+					  </NavItem>
+					  <NavItem>
+						<NavLink active href="/yelp">
+						  Yelp
+						</NavLink>
+					  </NavItem>
+					</Nav>
+			</Collapse>
+			</div>
+		</Navbar>
+		
     );
   }
 }
